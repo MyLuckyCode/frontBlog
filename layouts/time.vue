4f2c43c69921ecdf9d7bd69c243bfa6e218ed32d
VIP97444 <245156724@qq.com>
2019-06-05 16:25:59 +0800

    新增鼠标左右键点击效果

--- layouts/time.vue
+++ layouts/time.vue
@@ -1,26 +1,39 @@
 <template>
-    <div class="body">
+    <div class="body" @click="leftKey($event)" @contextmenu.prevent="rightkey($event)">
         <Header></Header>
         <div class="main" ref="main">
             <nuxt></nuxt>
         </div>
         <Footer></Footer>
+		<Heart ref="heart"></Heart>
+		<Ment ref="ment"></Ment>
     </div>
 </template>
 
 <script type="text/ecmascript-6">
     import Header from '~/components/header';
     import Footer from '~/components/footer';
+	import Heart from '~/components/heart.vue';
+	import Ment from '~/components/ment.vue';
     export default{
         components:{
-            Header,Footer
+            Header,Footer,Heart,Ment
         },
         async created(){
             this.$nextTick(()=>{
                 this.$refs.main.style.minHeight=(document.documentElement.clientHeight-60-150)+'px';
             })
 
-        }
+        },
+		methods:{
+			leftKey(event){
+			  this.$refs.heart.leftKey(event);
+			  this.$refs.ment.rightKey(1);
+			},
+			rightkey(event){
+			  this.$refs.ment.rightKey(event);
+			}
+		}
     }
 
 </script>
