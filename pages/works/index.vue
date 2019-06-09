8fc92faed89613e7b4085aaf2728b8b4936e9da1
VIP97444 <245156724@qq.com>
2019-06-09 16:17:40 +0800

    版面微改

--- pages/works/index.vue
+++ pages/works/index.vue
@@ -4,7 +4,6 @@
             <div class="item" v-for="item in list" @click="goUrl(item.target)">
                 <img :src="item.img" alt="">
                 <div class="hover-div">
-                    <h3>{{item.title}}</h3>
                     <p>{{item.info}}</p>
                 </div>
             </div>
@@ -29,13 +28,12 @@
         },
         methods:{
             goUrl(target){
-                if(target==''){
+                if(target=='javascript:;'){
                     alert('该项目目前没有在线链接');
                     return;
                 }
 
                 window.open(target,"_blank");
-                console.log(target)
             }
         },
         layout:'info'
@@ -43,20 +41,24 @@
 </script>
 
 <style scoped>
+    .container{
+        padding-top:10px;
+    }
     .works{
-        height:700px;
+
         width:100%;
         border-radius: 4px;
         background:#fff;
-        padding-top:20px;
+        padding:15px;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
     }
     .works .item{
-        float:left;
-        margin-right:82.5px;
         width:345px;
         height:200px;
         cursor: pointer;
-        margin-bottom: 30px;
+        margin-bottom: 40px;
         background: red;
         border-radius: 8px;
         overflow: hidden;
@@ -66,21 +68,14 @@
     .works .item:nth-child(3n){
         margin-right:0;
     }
-    .works .item:hover{
-        transform:scale(1.1);
-    }
-    .works .item .hover-div:hover h3{
-        opacity: 0;
-        filter:alpha(opacity=0);
-        transform:translateY(60px);
-    }
     .works .item .hover-div:hover p{
         opacity: 1;
+        background:red;
         filter:alpha(opacity=100);
-        transform:translateY(60px);
+        transform:translateY(80px);
     }
     .works .item:hover .hover-div{
-        background: rgba(0,0,0,0.7);
+        background: rgba(0,0,0,0.4);
     }
     .works .item img{
         width:345px;
@@ -92,20 +87,12 @@
     .works .item .hover-div{
         width:345px;
         height:200px;
-        background: rgba(0,0,0,0.4);
+        background: rgba(0,0,0,0);
         position:absolute;
         top:0;
         left:0;
         transition: 1s;
         text-align: center;
-    }
-    .works .item .hover-div h3{
-        transform:translateY(90px);
-        margin:0;
-        transition:1s;
-        opacity: 1;
-        filter:alpha(opacity=100);
-        color:#fff;
     }
     .works .item .hover-div p{
         padding:0 50px;
