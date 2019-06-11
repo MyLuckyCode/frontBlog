73e23ac492a61e76ba906ea08bd399c608884488
VIP97444 <245156724@qq.com>
2019-06-11 15:02:24 +0800

    修改简介

--- components/sidebar/Introduce.vue
+++ components/sidebar/Introduce.vue
@@ -1,11 +1,8 @@
 <template>
     <div class="introduce">
         <span>博客简介</span>
-
-        <!--<video id="my-video_html5_api" width="308" height="173" controls preload="none" class="vjs-tech" style="margin-top:0" @click.stop autoplay>-->
-            <!--<source src="http://localhost/smarty/admin_blog/upload/video/web.mp4" type="video/mp4">-->
-        <!--</video>-->
-        <p class="content">唯品秀博客: weipxiu.com，一个关注Web前端开发技术、关注用户体验、坚持更多原创实战教程的个人网站，愿景：成为宇宙中最具有代表性的前端博客，期待您的参与</p>
+        <p class="content">幸运码博客主要是记录web开发过程中的坑、笔记、案例的地方，偶尔也是抒发自己情感的地方。</p>
+        <p class="content"><a href="https://github.com/MyLuckyCode/frontBlog" target="_blank"><i class="icon-14 iconfont">&#xe709;</i>stars</a></p>
     </div>
 </template>
 
@@ -47,6 +44,12 @@
     font-size:14px;
     color:rgb(115,115,115);
     line-height:150%;
+}
+.introduce p a{
+    display: inline-block;
+    margin-top:10px;
+    padding:0 5px;
+    border:1px solid #efefef;
 }
 
 </style>
